50ca9fa6dfb1a3577c43fba759079694182a0ec1
luizTech01 <luizflavio010@gmail.com>
2024-12-30 15:28:02 -0300

    feature/ adicionado novos cenario de erro no cadastro

--- run/test/specs/casos-de-erro/cadastro.e2e.js
+++ run/test/specs/casos-de-erro/cadastro.e2e.js
@@ -12,7 +12,7 @@ async function digitarComAtraso(campo, texto, atraso) {
     }
 }
 
-describe('Cenarios de Login', () => {
+describe('Cenarios de Cadastro', () => {
     
     beforeEach(async () => {
         await browser.url('https://front.serverest.dev/login');
@@ -50,4 +50,100 @@ describe('Cenarios de Login', () => {
         }
     })
 
+    it('Tentar cadastrar sem uma senha', async () => {
+        const cadastroAdm = await $("[data-testid='cadastrar']");
+        await cadastroAdm.click();
+
+        await pause(3000);
+
+        const campoNome = await $(`[data-testid='nome']`);
+        await digitarComAtraso(campoNome, 'Nome Teste da Silva', 100);
+          
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+
+        const cadastrar= await $("[data-testid='cadastrar']");
+        await cadastrar.click();
+
+        await pause(3000);
+
+        const mensagemErro = await browser.$(`//*[@id="root"]/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErro, 'Password é obrigatório');
+        
+        await pause(3000);
+        
+    })
+
+    it('Tentar cadastrar sem um nome', async () => {
+        const cadastroAdm = await $("[data-testid='cadastrar']");
+        await cadastroAdm.click();
+
+        await pause(3000);
+        
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+
+        const campoSenha = await $(`[data-testid='password']`);
+        await digitarComAtraso(campoSenha, 'senhafacil123', 100);
+
+        const cadastrar= await $("[data-testid='cadastrar']");
+        await cadastrar.click();
+
+        await pause(3000);
+
+        const mensagemErro = await browser.$(`//*[@id="root"]/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErro, 'Nome é obrigatório');
+        
+        await pause(3000);
+        
+    })
+
+    it('Tentar cadastrar sem um email', async () => {
+        const cadastroAdm = await $("[data-testid='cadastrar']");
+        await cadastroAdm.click();
+
+        await pause(3000);
+        
+        const campoNome = await $(`[data-testid='nome']`);
+        await digitarComAtraso(campoNome, 'Nome Teste da Silva', 100);
+
+        const campoSenha = await $(`[data-testid='password']`);
+        await digitarComAtraso(campoSenha, 'senhafacil123', 100);
+
+        const cadastrar= await $("[data-testid='cadastrar']");
+        await cadastrar.click();
+
+        await pause(3000);
+
+        const mensagemErro = await browser.$(`//*[@id="root"]/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErro, 'Email é obrigatório');
+        
+        await pause(3000);
+        
+    })
+
+
+    it('Tentar cadastrar sem um email', async () => {
+        const cadastroAdm = await $("[data-testid='cadastrar']");
+        await cadastroAdm.click();
+
+        await pause(3000);
+
+        const cadastrar= await $("[data-testid='cadastrar']");
+        await cadastrar.click();
+
+        await pause(3000);
+
+        const mensagemErroNome = await browser.$(`//*[@id="root"]/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroNome, 'Nome é obrigatório');
+        
+        const mensagemErroEmail = await browser.$(`//*[@id="root"]/div/div/form/div[2]/span`).getText();
+            assert.strictEqual(mensagemErroEmail, 'Email é obrigatório');
+
+        const mensagemErroPassword = await browser.$(`//*[@id="root"]/div/div/form/div[3]/span`).getText();
+            assert.strictEqual(mensagemErroPassword, 'Password é obrigatório');
+
+        await pause(3000);
+        
+    })
 })
