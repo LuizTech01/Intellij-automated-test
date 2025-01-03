ce9b8ea432f661ff931893c8e27c645a3252c9a7
luizTech01 <luizflavio010@gmail.com>
2025-01-03 19:30:54 -0300

    feature/ adicionando novos casos de erro para as funcionalidades

--- run/test/specs/casos-de-erro/funcionalidades.e2e.js
+++ run/test/specs/casos-de-erro/funcionalidades.e2e.js
@@ -155,9 +155,17 @@ describe('Cadastrar novos usuarios', () => {
 
         const mensagemErroSenha = await browser.$(`//*[@id="root"]/div/div/div/form/div[3]/span`).getText();
             assert.strictEqual(mensagemErroSenha, 'Password é obrigatório');
-    })
+    });
+});
 
-    it('Escluindo multiplas pessoas da lista', async () => {
+describe('Cadastro de produtos', () => {
+
+    beforeEach(async () => {
+        await browser.url('https://front.serverest.dev/login');
+        await pause(3000);
+    });
+
+    it('Adicionando produto sem quantidade', async () => {
         const campoEmail = await $('[name="email"]');
         await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
             
@@ -172,10 +180,168 @@ describe('Cadastrar novos usuarios', () => {
         const urlHome = 'https://front.serverest.dev/admin/home'
         expect(urlHome).toBe('https://front.serverest.dev/admin/home');
     
-        const cadastroNovoUsuario = await $("[data-testid='listarUsuarios']");
+        const cadastroNovoUsuario = await $("[data-testid='cadastrarProdutos']");
         await cadastroNovoUsuario.click();
     
         await pause(3000); 
-    })
 
-})
+        const campoCadastroNome = await $("[data-testid='nome']");
+        await digitarComAtraso(campoCadastroNome, 'Caneta', 100);
+        
+        const campoPrecoProduto = await $('#price');
+        await digitarComAtraso(campoPrecoProduto, '1', 100);
+
+        const campoDescricaoProduto = await $('#description');
+        await digitarComAtraso(campoDescricaoProduto, 'Uma caneta azul comum', 100);
+
+        const cadastroProduto = await $("[data-testid='cadastarProdutos']");
+        await cadastroProduto.click();
+
+        const mensagemErroQuantidade = await browser.$(`//*[@id="root"]/div/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroQuantidade, 'Quantidade é obrigatório');
+    });
+
+    it('Adicionando produto sem descricao', async () => {
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+            
+        const campoSenha = await $('#password');
+        await digitarComAtraso(campoSenha, 'dasilvasenha123', 100);
+            
+        const cadastroAdm = await $("[data-testid='entrar']");
+        await cadastroAdm.click();
+    
+        await pause(3000);
+    
+        const urlHome = 'https://front.serverest.dev/admin/home'
+        expect(urlHome).toBe('https://front.serverest.dev/admin/home');
+    
+        const cadastroNovoUsuario = await $("[data-testid='cadastrarProdutos']");
+        await cadastroNovoUsuario.click();
+    
+        await pause(3000); 
+
+        const campoCadastroNome = await $("[data-testid='nome']");
+        await digitarComAtraso(campoCadastroNome, 'Caneta', 100);
+        
+        const campoPrecoProduto = await $('#price');
+        await digitarComAtraso(campoPrecoProduto, '1', 100);
+
+        const campoDescricaoProduto = await $('[data-testid="quantity"]');
+        await digitarComAtraso(campoDescricaoProduto, '5', 100);
+
+        const cadastroProduto = await $("[data-testid='cadastarProdutos']");
+        await cadastroProduto.click();
+
+        const mensagemErroQuantidade = await browser.$(`//*[@id="root"]/div/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroQuantidade, 'Descricao é obrigatório');
+    });
+
+    it('Adicionando produto sem preco', async () => {
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+            
+        const campoSenha = await $('#password');
+        await digitarComAtraso(campoSenha, 'dasilvasenha123', 100);
+            
+        const cadastroAdm = await $("[data-testid='entrar']");
+        await cadastroAdm.click();
+    
+        await pause(3000);
+    
+        const urlHome = 'https://front.serverest.dev/admin/home'
+        expect(urlHome).toBe('https://front.serverest.dev/admin/home');
+    
+        const cadastroNovoUsuario = await $("[data-testid='cadastrarProdutos']");
+        await cadastroNovoUsuario.click();
+    
+        await pause(3000); 
+
+        const campoCadastroNome = await $("[data-testid='nome']");
+        await digitarComAtraso(campoCadastroNome, 'Caneta', 100);
+        
+        const campoPrecoProduto = await $('#description');
+        await digitarComAtraso(campoPrecoProduto, 'Caneta azul comum', 100);
+
+        const campoDescricaoProduto = await $('[data-testid="quantity"]');
+        await digitarComAtraso(campoDescricaoProduto, '5', 100);
+
+        const cadastroProduto = await $("[data-testid='cadastarProdutos']");
+        await cadastroProduto.click();
+
+        const mensagemErroQuantidade = await browser.$(`//*[@id="root"]/div/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroQuantidade, 'Preco é obrigatório');
+    });
+
+    it('Adicionando produto sem nome', async () => {
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+            
+        const campoSenha = await $('#password');
+        await digitarComAtraso(campoSenha, 'dasilvasenha123', 100);
+            
+        const cadastroAdm = await $("[data-testid='entrar']");
+        await cadastroAdm.click();
+    
+        await pause(3000);
+    
+        const urlHome = 'https://front.serverest.dev/admin/home'
+        expect(urlHome).toBe('https://front.serverest.dev/admin/home');
+    
+        const cadastroNovoUsuario = await $("[data-testid='cadastrarProdutos']");
+        await cadastroNovoUsuario.click();
+    
+        await pause(3000); 
+
+        const campoCadastroNome = await $("[data-testid='preco']");
+        await digitarComAtraso(campoCadastroNome, '2', 100);
+        
+        const campoPrecoProduto = await $('#description');
+        await digitarComAtraso(campoPrecoProduto, 'Caneta azul comum', 100);
+
+        const campoDescricaoProduto = await $('[data-testid="quantity"]');
+        await digitarComAtraso(campoDescricaoProduto, '5', 100);
+
+        const cadastroProduto = await $("[data-testid='cadastarProdutos']");
+        await cadastroProduto.click();
+
+        const mensagemErroQuantidade = await browser.$(`//*[@id="root"]/div/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroQuantidade, 'Nome é obrigatório');
+    });
+
+    it('Adicionando produto sem preencher nada', async () => {
+        const campoEmail = await $('[name="email"]');
+        await digitarComAtraso(campoEmail, 'testedasilv@gmail.com', 100);
+            
+        const campoSenha = await $('#password');
+        await digitarComAtraso(campoSenha, 'dasilvasenha123', 100);
+            
+        const cadastroAdm = await $("[data-testid='entrar']");
+        await cadastroAdm.click();
+    
+        await pause(3000);
+    
+        const urlHome = 'https://front.serverest.dev/admin/home'
+        expect(urlHome).toBe('https://front.serverest.dev/admin/home');
+    
+        const cadastroNovoUsuario = await $("[data-testid='cadastrarProdutos']");
+        await cadastroNovoUsuario.click();
+    
+        await pause(3000); 
+
+        const cadastroProduto = await $("[data-testid='cadastarProdutos']");
+        await cadastroProduto.click();
+
+        const mensagemErroNome = await browser.$(`//*[@id="root"]/div/div/div/form/div[1]/span`).getText();
+            assert.strictEqual(mensagemErroNome, 'Nome é obrigatório');
+        
+        const mensagemErroPreco = await browser.$(`//*[@id="root"]/div/div/div/form/div[2]/span`).getText();
+            assert.strictEqual(mensagemErroPreco, 'Preco é obrigatório');
+
+        const mensagemErroDescricao = await browser.$(`//*[@id="root"]/div/div/div/form/div[3]/span`).getText();
+            assert.strictEqual(mensagemErroDescricao, 'Descricao é obrigatório');
+
+        const mensagemErroQuantidade = await browser.$(`//*[@id="root"]/div/div/div/form/div[4]/span`).getText();
+            assert.strictEqual(mensagemErroQuantidade, 'Quantidade é obrigatório');
+    });
+});
